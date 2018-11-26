7c23d06eebabaf462c1130625613fd5179f43524
André Dias <andre.francisco.dias@gmail.com>
2018-11-26 10:57:42 +0000

    Fim de Aula 26/11 (1ª Aula)

--- Trabalho_Final_PM_Projeto/main.c
+++ Trabalho_Final_PM_Projeto/main.c
@@ -6,39 +6,83 @@ void showStock(){
     int id;
     char type[9];
     double price;
-    char input[255];
+    int totalMateriais[6] = {0};
+    double totalPreco = 0;
+    char materiais[6][9];
 
+    //Abrir o ficheiro
     FILE *fp;
     fp = fopen("stock.txt", "r");
-    if(fp == NULL){
+    if(fp == NULL){         //Verificar se o ficheiro existe
         printf("Ficheiro Nao lido.\n");
     }
-    while(fscanf(fp, "%d %s %lf", &id, type, &price)!=EOF){
+    while(fscanf(fp, "%d %s %lf", &id, type, &price)!=EOF){     //Ler o Ficheiro
         printf("%d  %s  %.2lf\n", id, type, price);
+
+        totalMateriais[id-1]++;
+        totalPreco += price;
+        strcpy(materiais[id-1], type);
     }
 
+    printf("\n");
+
+    for (int i=0; i<6; i++){
+        printf("Existem %d materiais de %s\n", totalMateriais[i], materiais[i]);      //Imprimir a soma dos materiais e o respetivo tipo
+    }
+
+    printf("\nO valor total do stock e: %.2lf\n\n", totalPreco);
     fclose(fp);
+
+    system(" PAUSE "); // Esperar por input do utilizador
+    menu();
 }
 
 
 //Establish menu
 void menu(){
+    system("@cls||clear"); //Limpar o ecra
     char opcao=' ';
 
-    printf("    ****Factory Admin Menu****\n");
-    printf("\n  1 - Show Stock\n");
-    printf("  2 - Refill Stock\n");
-    printf("  3 - Build Car\n");
-    printf("  4 - Factory State\n");
-    printf("  5 - Statistics\n");
-    printf("  6 - Time Skip\n");
-    printf("  E - Exit\n");
+    printf("    ****Menu de Administrador da Fabrica****\n");
+    printf("\n  1 - Mostrar Stock\n");
+    printf("  2 - Aumentar Stock\n");
+    printf("  3 - Construir Carro\n");
+    printf("  4 - Estado da Fabrica\n");
+    printf("  5 - Estatisticas\n");
+    printf("  6 - Passar Tempo\n");
+    printf("  S - Sair\n");
     printf("\nOption: ");
     scanf(" %c", &opcao);
 
     switch(opcao){
         case '1':
             showStock();
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        case '6':
+            break;
+        case 'S':
+            printf("Obrigado por usar o nosso programa :D\n\n");
+            system(" PAUSE ");
+            exit(1);
+            break;
+        case 's':
+            printf("Obrigado por usar o nosso programa :D\n\n");
+            system(" PAUSE ");
+            exit(1);
+            break;
+        default:
+            printf("Opcao Invalida.\n");
+            system(" PAUSE ");
+            menu();
+            break;
     }
 }
 
